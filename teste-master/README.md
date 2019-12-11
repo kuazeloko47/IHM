1814c1a8b5d21f9f08510ae0f575935cd23b2755
kuazeloko47 <58788481+kuazeloko47@users.noreply.github.com>
2019-12-11 16:16:17 -0300

    Add files via upload

---
+++ teste-master/README.md
@@ -0,0 +1,7 @@
+---
+noteId: "bc5e307017aa11ea84828fad01de28b2"
+tags: []
+
+---
+
+# teste
---
+++ teste-master/view/modal.php
@@ -0,0 +1,48 @@
+<!-- Modal -->
+<div class="modal fade" id="ExemploModalCentralizado" tabindex="-1" role="dialog" aria-labelledby="TituloModalCentralizado" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h3 class="modal-title" id="TituloModalCentralizado">Cadastrar</h3>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form action="">
+                    <div class="container">
+                        <div class="row form-group">
+                            <div class="col-md-12">
+                                <label for="">Título</label>
+                                <input type="text" name="" id="">
+                            </div>
+                        </div>
+                        <div class="row form-group">
+                            <div class="col-md-12">
+                                <label for="">Autor</label>
+                                <input type="text" name="" id="">
+                            </div>
+                        </div>
+                        <div class="row form-group">
+                            <div class="col-md-12">
+                                <label for="">Descrição</label>
+                                <input type="text" name="" id="">
+                            </div>
+                        </div>
+                        <div class="row form-group">
+                            <div class="col-md-12">
+                                <label for="">ISBN</label>
+                                <input type="text" name="" id="">
+                            </div>
+                        </div>
+
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Fechar</button>
+                <button type="button" class="btn btn-info">Salvar mudanças</button>
+            </div>
+        </div>
+    </div>
+</div>
